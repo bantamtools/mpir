efe2d0864eaa1e2ec9d17913c29a225ee2f0b50c
James Beckman <jsb.programming@gmail.com>
2015-08-28 17:18:03 -0700

    set libdir specifically so that the install_name will point at the directory where the created libs actually reside, and not at /opt/local/...

--- metaBuild.sh
+++ metaBuild.sh
@@ -1,15 +1,17 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PREFIX=$DIR/.libs
+
 if [ "$1" = "build" ]; then
   SYSTEM_NAME=`uname -s` 
   if [ "$SYSTEM_NAME" = "Darwin" ]; then
     echo "Building for OSX"
     (cd $DIR && \
-      ./configure CXX='clang++ -std=c++11 -stdlib=libc++' --enable-cxx --enable-gmpcompat --enable-shared --disable-static && \
-      make)
+    ./configure --libdir=$PREFIX CXX='clang++ -std=c++11 -stdlib=libc++' --enable-cxx --enable-gmpcompat --disable-static --enable-shared && \
+    make)
   elif echo "$SYSTEM_NAME" | grep -q "MINGW64_NT"; then
     echo "Building for Windows"
     (cd $DIR && \
-    ./configure --host=corei7-w64-mingw64 --enable-fat --enable-cxx --enable-gmpcompat --enable-shared --disable-static && \
+    ./configure --libdir=$PREFIX --host=corei7-w64-mingw64 --enable-fat --enable-cxx --enable-gmpcompat --disable-static --enable-shared && \
     make)
   elif echo "$SYSTEM_NAME" | grep -q "MSYS"; then
     echo "ERROR: the MSYS shell is not supported. Please use the MinGW-w64 Win64 Shell instead."
